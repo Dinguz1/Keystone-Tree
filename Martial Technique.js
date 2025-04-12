2e6c61db4f44944c4bdb3674b361dc4cb57da677
Dinguz1 <GabrielAugustine@protonmail.com>
2025-04-12 16:35:07 -0400

    cifcle

--- Martial Technique.js
+++ Martial Technique.js
@@ -1,8 +1,8 @@
 
 window.addEventListener("DOMContentLoaded", () => {
 
-  new LeaderLine(skill1, skill2, { color: '#999', path: 'straight', endPlug: 'behind'});
-  new LeaderLine(skill1, skill3, { color: '#999'});
+  new LeaderLine(skill1, skill2, { color: '#999', path: 'straight', endPlug: 'behind', shape: 'circle'});
+  new LeaderLine(skill1, skill3, { color: '#999', path: 'straight', endPlug: 'behind', shape: 'circle'});
 
 
   });
