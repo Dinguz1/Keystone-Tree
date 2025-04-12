d4df916293b8afa5659ead3d8b83b11ecf2704ff
Dinguz1 <GabrielAugustine@protonmail.com>
2025-04-12 18:49:06 -0400

    lines

--- Martial Technique.js
+++ Martial Technique.js
@@ -1,7 +1,8 @@
 
 window.addEventListener("DOMContentLoaded", () => {
 
-  // new LeaderLine(skill1, skill2, { color: '#999', path: 'straight', endPlug: 'behind', shape: 'circle'});
-  // new LeaderLine(skill1, skill3, { color: '#999', path: 'straight', endPlug: 'behind', shape: 'circle'});
-
+  new LeaderLine(rsk, sk2, { color: '#999', path: 'straight', shape: 'circle', startSocket: 'bottom', endSocket: 'top', endPlug: 'behind', path: 'straight'});
+  new LeaderLine(rsk, sk3, { color: '#999', path: 'straight', shape: 'circle', startSocket: 'bottom', endSocket: 'top', endPlug: 'behind', path: 'straight'});
+  new LeaderLine(sk2, sk4, { color: '#999', path: 'straight', shape: 'circle', startSocket: 'bottom', endSocket: 'top', endPlug: 'behind', path: 'straight'});
+  
 });
