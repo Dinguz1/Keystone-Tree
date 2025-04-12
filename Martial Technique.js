ab8a431648ddea84dd627bee2ee5301f4cedbafc
Dinguz1 <GabrielAugustine@protonmail.com>
2025-04-12 18:25:48 -0400

    broked

--- Martial Technique.js
+++ Martial Technique.js
@@ -1,8 +1,7 @@
 
 window.addEventListener("DOMContentLoaded", () => {
 
-  new LeaderLine(skill1, skill2, { color: '#999', path: 'straight', endPlug: 'behind', shape: 'circle'});
-  new LeaderLine(skill1, skill3, { color: '#999', path: 'straight', endPlug: 'behind', shape: 'circle'});
+  // new LeaderLine(skill1, skill2, { color: '#999', path: 'straight', endPlug: 'behind', shape: 'circle'});
+  // new LeaderLine(skill1, skill3, { color: '#999', path: 'straight', endPlug: 'behind', shape: 'circle'});
 
-
-  });
+});
