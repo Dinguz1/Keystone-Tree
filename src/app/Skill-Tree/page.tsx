db32e2631f986b22f0b801ed1ac7eaba28b99d50
Dinguz1 <GabrielAugustine@protonmail.com>
2025-04-13 21:23:04 -0400

    Fixed Martial Path

--- src/app/Skill-Tree/page.tsx
+++ src/app/Skill-Tree/page.tsx
@@ -4,7 +4,7 @@ export default function SkillTreePage() {
     return (
         <main> 
             <h1><u>Skill tree selection page</u></h1>
-            <Button text="Martial" href="skill-tree/martial"/>
+            <Button text="Martial" href="Skill-Tree/Martial"/>
             <Button text="Home" href="/"/>
         </main>
     )
