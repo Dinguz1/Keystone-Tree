317be2c46e85424fe05f07f06dcbafcb1fa63ae3
Dinguz1 <GabrielAugustine@protonmail.com>
2025-04-14 01:14:48 -0400

    fixing homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,8 @@ export default function Home() {
 
       <h1><u>Project Keystone Home</u></h1>
 
-      <p>
-        Keystone is ultimately a system in which stories are told. These stories are a 
-        collaboration between the Game Master (GM) and players. The GM presents a world 
-        in which the players  tell their story. This includes providing locations, 
-        adventure hooks, and Non-Player Characters (NPCs) that the players can engage with. 
-        The Keystone system tries to facilitate stories that ride on a knife's edge, with 
-        fast paced and punishing combat as well as significant consequences for poor decisions 
-        but a plethora of options and abilities both in and out of combat to triumph over obstacles. 
-        At any moment disaster may strike or victory may be snatched from the jaws of defeat. </p>
+
+
       
 
     
