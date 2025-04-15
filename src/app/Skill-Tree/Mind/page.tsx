1878926b8a0b96d3b19721a75f14cedbc931931b
NeptuneRjo <rjo9000@outlook.com>
2025-04-15 16:52:41 -0400

    Created page for skill-tree/mind

---
+++ src/app/Skill-Tree/Mind/page.tsx
@@ -0,0 +1,12 @@
+import MindContent from "./components/MindContent";
+
+export default function Page() {
+    return (
+        <main className="flex flex-col items-center content-center max-h-screen">
+            <h1>
+                <u>Martial Skill Tree</u>
+            </h1>
+            <MindContent />
+        </main>
+    );
+}
