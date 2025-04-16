d9abc011bf17fd14e2017951cbcb9e8d5bf3a277
Dinguz1 <GabrielAugustine@protonmail.com>
2025-04-16 19:17:27 -0400

    Changed Metadata title and des in layout and added indent to p in css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
         {MainNavBar()}
         <h1 className="text-gradient">Keystone Home Page</h1>
         <p>
-        {"Keystone is ultimately a system in which stories are told. These stories are a collaboration between the Game Master (GM) and players." +
-        "The GM presents a world in which the players  tell their story. This includes providing locations, adventure hooks, and Non-Player " +
+        {"Keystone is a TTRPG system in which stories are told. These stories are a collaboration between the Game Master (GM) and players." +
+        "The GM presents a world in which the players tell their story. This includes providing locations, adventure hooks, and Non-Player " +
         "Characters (NPCs) that the players can engage with. The Keystone system facilitates stories that ride on a knife's edge, with fast" +
-         "paced  and punishing combat, as well as significant consequences for poor decisions but a plethora of options and abilities both in "+
+         "paced and punishing combat, as well as significant consequences for poor decisions but a plethora of options and abilities both in "+
          "and out of combat to triumph over obstacles. At any moment, disaster may strike or victory may be snatched from the jaws of defeat. " }
         </p>
         </main>
