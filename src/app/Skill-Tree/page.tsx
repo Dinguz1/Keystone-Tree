a289741bee5b0d5d8dbdd32c7a27ba9510a65782
Dinguz1 <GabrielAugustine@protonmail.com>
2025-04-15 23:58:49 -0400

    moved Suspense

--- src/app/Skill-Tree/page.tsx
+++ src/app/Skill-Tree/page.tsx
@@ -1,14 +1,12 @@
 import Button from "@/components/Button";
-import { Suspense } from "react";
+
 
 export default function SkillTreePage() {
     return (
         <main> 
             <h1><u>Skill tree selection page</u></h1>
-            <Suspense>
-                <Button text="Martial" href="Skill-Tree/Martial"/>
-                <Button text="Home" href="/"/>
-            </Suspense>
+            <Button text="Martial" href="Skill-Tree/Martial"/>
+            <Button text="Home" href="/"/>
         </main>
     )
 }
