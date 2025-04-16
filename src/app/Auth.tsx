f661a902b678f45923b68f14238e12fa8ac4ea1d
Dinguz1 <GabrielAugustine@protonmail.com>
2025-04-16 02:06:45 -0400

    Removed unused const isAuth

--- src/app/Auth.tsx
+++ src/app/Auth.tsx
@@ -7,7 +7,6 @@ export default function Auth(){
     const searchParams = useSearchParams();
     const givenId = searchParams.get('PlayerID');
     const idList = ["Guest1597!$", "692338DM"]
-    const isAuth = (givenId && idList.includes(givenId));
     
     return (typeof givenId === 'string' && idList.includes(givenId))
 
