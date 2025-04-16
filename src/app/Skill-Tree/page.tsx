eea09bec25329a124a250663d589c3ab3ade2a10
NeptuneRjo <rjo9000@outlook.com>
2025-04-15 22:41:00 -0400

    Added suspense wrapped to page using Button

--- src/app/Skill-Tree/page.tsx
+++ src/app/Skill-Tree/page.tsx
@@ -1,11 +1,14 @@
 import Button from "@/components/Button";
+import { Suspense } from "react";
 
 export default function SkillTreePage() {
     return (
         <main> 
             <h1><u>Skill tree selection page</u></h1>
-            <Button text="Martial" href="Skill-Tree/Martial"/>
-            <Button text="Home" href="/"/>
+            <Suspense>
+                <Button text="Martial" href="Skill-Tree/Martial"/>
+                <Button text="Home" href="/"/>
+            </Suspense>
         </main>
     )
 }
