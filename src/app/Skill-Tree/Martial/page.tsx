71cef12528dfa0eb0f624be24470aa3d878239f6
Dinguz1 <GabrielAugustine@protonmail.com>
2025-04-16 02:33:15 -0400

    updated Marial Page to new format

--- src/app/Skill-Tree/Martial/page.tsx
+++ src/app/Skill-Tree/Martial/page.tsx
@@ -1,34 +1,12 @@
-import SkillTree from "@/components/SkillTree";
-import Skill from "@/components/Skill";
+import MindContent from "./components/MartialContent";
 
-export default function MartialPage() {
-
-    const skills = [
-
-        {name: "Skill 1", description: "Description 1", gridColumnIn: 1, gridRowIn: 1},
-        {name: "Skill 2", description: "Description 2", gridColumnIn: 2, gridRowIn: 1},
-        
-    ];
-
+export default function Page() {
     return (
-        <main style={{ 
-            display: "flex", 
-            flexDirection: "column", 
-            alignItems: "center", 
-            justifyContent: "center",  }}> 
-
-            <h1><u>Martial Skill Tree</u></h1>
-            
-            <SkillTree skills={skills.map(skill => (
-                <Skill 
-                    key={skill.name}
-                    name={skill.name} 
-                    description={skill.description} 
-                    gridColumnIn={skill.gridColumnIn} 
-                    gridRowIn={skill.gridRowIn} 
-                />
-            ))} />
-
+        <main className="flex flex-col items-center content-center max-h-screen">
+            <h1>
+                <u>Martial Skill Tree</u>
+            </h1>
+            <MindContent />
         </main>
-    )
+    );
 }
--- src/app/Skill-Tree/Mind/page.tsx
+++ src/app/Skill-Tree/Mind/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <main className="flex flex-col items-center content-center max-h-screen">
             <h1>
-                <u>Martial Skill Tree</u>
+                <u>Mind Skill Tree</u>
             </h1>
             <MindContent />
         </main>
