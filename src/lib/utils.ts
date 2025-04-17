963942e726fb714660ce5ed1e2d13287a858cec8
NeptuneRjo <rjo9000@outlook.com>
2025-04-16 20:46:43 -0400

    Created utility method for getting auth level of id

---
+++ src/lib/utils.ts
@@ -0,0 +1,16 @@
+/**
+ * Returns an index depending on the matching ID-
+ * If there are no matching IDs, returns -1
+ * @param id 
+ * @returns number
+ */
+export const getAuthLevel = (id: string | null): number => {
+    switch (id) {
+        case '1235':
+            return 1
+        case '1235GM':
+            return 2
+        default:
+            return -1
+    }
+}
