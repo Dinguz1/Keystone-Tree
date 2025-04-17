cbd4b07d33e5faeaf9b80ca3b1421c3201097e18
NeptuneRjo <rjo9000@outlook.com>
2025-04-16 21:00:20 -0400

    Organized content into single folder and updated imports

--- src/app/Lore/page.tsx
+++ src/app/Lore/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Auth from "@/app/Auth";
-import BaseLore from "@/Basic Content/BaseLore"
-import AdvLore from "@/Adv Content/AdvLore";
+import BaseLore from "@/content/basic/BaseLore"
+import AdvLore from "@/content/advanced/AdvLore";
 
 export default function Lore() {
     if (Auth() == 1){
--- src/app/Rules/page.tsx
+++ src/app/Rules/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import BaseRules from "@/Basic Content/BaseRules";
+import BaseRules from "@/content/basic/BaseRules";
 
 export default function Rules() {
     return <BaseRules />
