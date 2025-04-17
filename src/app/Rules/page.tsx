1e1a1c11bc1ca4c4998b675333624df09212af26
NeptuneRjo <rjo9000@outlook.com>
2025-04-16 20:34:45 -0400

    Removed login component and updated content to use HTMX

--- src/app/Rules/page.tsx
+++ src/app/Rules/page.tsx
@@ -1,12 +1,6 @@
 'use client'
-import Auth from "@/app/Auth";
-import LogIn from "../LogIn";
 import BaseRules from "@/Basic Content/BaseRules";
 
 export default function Rules() {
-    if (Auth() == -1){
-        return LogIn()
-    } else{
-        return BaseRules()
-    }
+    return <BaseRules />
 }
