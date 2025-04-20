7afb4eb36ae32c1609e8087270d794b436175c5b
Dinguz1 <GabrielAugustine@protonmail.com>
2025-04-19 20:02:26 -0400

    added more filler text and made some formating changes

--- src/app/DeadEndPage/page.tsx
+++ src/app/DeadEndPage/page.tsx
@@ -1,7 +1,7 @@
 export default function SkillTreePage() {
     return (
         <div className="stan-width"> 
-            <h1 className="text-gradient"> This Page has not yet been made, please return home </h1>
+            <h1 className="text-center"> This Page has not yet been made, please return home </h1>
         </div>
     )
 }
--- src/content/basic/BaseLore.tsx
+++ src/content/basic/BaseLore.tsx
@@ -5,13 +5,52 @@ export default function BaseLore() {
 
         <div className = "stan-width">
             <h1>Base Lore</h1>
-
+            <hr className="break"></hr>
             <p>{"This is the basic lore page. It is the lore that any player ID will see. Some Lore bits like names " +
             "will have short blurbs attached to them, these should look like "}
             <Tooltip content="I am an example tooltip"><a>This</a></Tooltip>
-            {" so that players can get more information."} 
+            {" so that players can get more information.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed est et neque vulputate varius." + 
+            " Suspendisse nibh nisi, porta sed nisi eget, tristique pretium leo. Aliquam in feugiat justo. Vestibulum venenatis sed ante in malesuada. Cras " + 
+            "quam justo, faucibus non ipsum nec, sodales placerat lacus. Morbi dictum felis et semper condimentum. Aenean imperdiet ullamcorper sapien, non " + 
+            "mollis mi sodales ac. Vivamus volutpat mauris vitae feugiat posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per " + 
+            "inceptos himenaeos. Suspendisse eget tincidunt nunc, egestas egestas lorem. Aenean molestie metus vel placerat imperdiet. Morbi non ullamcorper " + 
+            "dui, sit amet tempus felis. In vehicula euismod risus in dignissim. Maecenas suscipit diam vitae gravida venenatis. Donec lectus tellus, malesuada " + 
+            "vel porttitor vel, eleifend in ex. Nunc ac libero eget sem placerat commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada " + 
+            "fames ac turpis egestas. Vestibulum sit amet ipsum non eros gravida convallis. Pellentesque fermentum lorem vitae lacinia venenatis. In tincidunt " + 
+            "justo sed pharetra suscipit. Praesent sed nisi at nulla cursus auctor non sit amet magna. Vivamus maximus tincidunt massa at molestie. Duis posuere " + 
+            "mauris neque, non tempus dolor feugiat eget. Mauris vel purus ligula. Praesent id lacus in mi luctus commodo ac ut eros. Nulla pharetra nulla eget " + 
+            "interdum venenatis. Proin lobortis tempor molestie. Quisque semper iaculis eros. Curabitur pretium lectus gravida efficitur lacinia. Duis non congue " + 
+            "ligula. Suspendisse lacinia enim dolor, quis hendrerit ipsum tristique sed. Maecenas elementum felis a velit congue volutpat. Aliquam porttitor lacus " + 
+            "pretium lacus mattis egestas. Proin in eros sem. Phasellus in arcu sit amet nunc pharetra vulputate a at enim. Nunc in velit condimentum, suscipit " + 
+            "erat commodo, vestibulum ipsum. Mauris sollicitudin diam sit amet condimentum gravida. Duis at ligula tincidunt, mollis risus a, elementum lacus. " + 
+            "Donec arcu justo, congue id leo id, sollicitudin malesuada sem. Sed nec imperdiet lorem. Morbi in condimentum augue, vel varius risus. Maecenas ac " + 
+            "tempus neque, quis facilisis magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla quis viverra nibh, eu ultrices dui. Sed at " + 
+            "quam non urna accumsan molestie in non lacus. Proin ut viverra sem. Pellentesque tristique consequat accumsan. Aliquam ut tincidunt magna. Morbi " + 
+            "imperdiet enim eu faucibus tristique. Etiam aliquet quis arcu et dictum. Vestibulum ligula tellus, cursus quis pharetra porta, posuere a nisl. " + 
+            "Aliquam sagittis sit amet augue eu pulvinar."} 
             </p>
-            
+            <h3>Subsection</h3>
+            <hr className="break"></hr>
+            <p> {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed est et neque vulputate varius." + 
+            " Suspendisse nibh nisi, porta sed nisi eget, tristique pretium leo. Aliquam in feugiat justo. Vestibulum venenatis sed ante in malesuada. Cras " + 
+            "quam justo, faucibus non ipsum nec, sodales placerat lacus. Morbi dictum felis et semper condimentum. Aenean imperdiet ullamcorper sapien, non " + 
+            "mollis mi sodales ac. Vivamus volutpat mauris vitae feugiat posuere. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per " + 
+            "inceptos himenaeos. Suspendisse eget tincidunt nunc, egestas egestas lorem. Aenean molestie metus vel placerat imperdiet. Morbi non ullamcorper " + 
+            "dui, sit amet tempus felis. In vehicula euismod risus in dignissim. Maecenas suscipit diam vitae gravida venenatis. Donec lectus tellus, malesuada " + 
+            "vel porttitor vel, eleifend in ex. Nunc ac libero eget sem placerat commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada " + 
+            "fames ac turpis egestas. Vestibulum sit amet ipsum non eros gravida convallis. Pellentesque fermentum lorem vitae lacinia venenatis. In tincidunt " + 
+            "justo sed pharetra suscipit. Praesent sed nisi at nulla cursus auctor non sit amet magna. Vivamus maximus tincidunt massa at molestie. Duis posuere " + 
+            "mauris neque, non tempus dolor feugiat eget. Mauris vel purus ligula. Praesent id lacus in mi luctus commodo ac ut eros. Nulla pharetra nulla eget " + 
+            "interdum venenatis. Proin lobortis tempor molestie. Quisque semper iaculis eros. Curabitur pretium lectus gravida efficitur lacinia. Duis non congue " + 
+            "ligula. Suspendisse lacinia enim dolor, quis hendrerit ipsum tristique sed. Maecenas elementum felis a velit congue volutpat. Aliquam porttitor lacus " + 
+            "pretium lacus mattis egestas. Proin in eros sem. Phasellus in arcu sit amet nunc pharetra vulputate a at enim. Nunc in velit condimentum, suscipit " + 
+            "erat commodo, vestibulum ipsum. Mauris sollicitudin diam sit amet condimentum gravida. Duis at ligula tincidunt, mollis risus a, elementum lacus. " + 
+            "Donec arcu justo, congue id leo id, sollicitudin malesuada sem. Sed nec imperdiet lorem. Morbi in condimentum augue, vel varius risus. Maecenas ac " + 
+            "tempus neque, quis facilisis magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla quis viverra nibh, eu ultrices dui. Sed at " + 
+            "quam non urna accumsan molestie in non lacus. Proin ut viverra sem. Pellentesque tristique consequat accumsan. Aliquam ut tincidunt magna. Morbi " + 
+            "imperdiet enim eu faucibus tristique. Etiam aliquet quis arcu et dictum. Vestibulum ligula tellus, cursus quis pharetra porta, posuere a nisl. " + 
+            "Aliquam sagittis sit amet augue eu pulvinar."}
+            </p>
         </div>
 
     )
