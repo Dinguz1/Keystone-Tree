a63a0ad312d2b3b2c96afb1cccccafc47424d5a5
NeptuneRjo <rjo9000@outlook.com>
2025-05-03 21:21:48 -0400

    Create content files for skill tree

---
+++ src/app/skill-tree/components/SkillTreeContent.tsx
@@ -0,0 +1,99 @@
+"use client";
+import Button from "@/components/Button";
+import { Edge, Handle, NodeProps, Position, ReactFlow } from "@xyflow/react";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+
+function TreeNode ({ data }: NodeProps) {
+    const searchParams = useSearchParams()
+    const id = searchParams.get('Player-ID')
+    const route = `/Skill-Tree/${data.text}?Player-ID=${id}`
+    
+    return (
+        <>
+            <Handle 
+                type="target"  
+                position={Position.Top} 
+                style={{ opacity: 0, visibility: 'hidden' }}
+                id="top-target"
+            />
+            <Handle 
+                type="source"  
+                position={Position.Top} 
+                style={{ opacity: 0, visibility: 'hidden' }}
+                id="top-source"
+            />
+            <Link href={route} className="skill">{data.text as string}</Link>
+            <Handle 
+                type="target"  
+                position={Position.Bottom} 
+                style={{ opacity: 0, visibility: 'hidden' }}
+                id="bottom-target"
+            />
+            <Handle 
+                type="source"  
+                position={Position.Bottom} 
+                style={{ opacity: 0, visibility: 'hidden' }}
+                id="bottom-source"
+            />
+        </>
+    )
+}
+
+const nodeTypes = {
+    treeNode: TreeNode
+}
+
+const SkillTreeContent = () => {
+    const trees = [
+        { 
+            id: '1', 
+            position: { x: 0, y: -260 }, 
+            data: { text: 'Martial' },
+            type: 'treeNode' 
+        },
+        { 
+            id: '2', 
+            position: { x: -130, y: -150 }, 
+            data: { text: 'Mind' },
+            type: 'treeNode' 
+        },
+        { 
+            id: '3', 
+            position: { x: -75, y: 0 }, 
+            data: { text: 'Divine' },
+            type: 'treeNode' 
+        },
+        { 
+            id: '4', 
+            position: { x: 75, y: 0 }, 
+            data: { text: 'Nature' }, 
+            type: 'treeNode'
+        },
+        { 
+            id: '5', 
+            position: { x: 130, y: -150 }, 
+            data: { text: 'Body' },
+            type: 'treeNode' 
+        },
+    ]
+
+    const edges: Edge[] = []
+
+    return (
+        <div className="flex items-center justify-center h-screen max-w-screen max-h-screen">
+            <ReactFlow 
+                nodes={trees}
+                nodeTypes={nodeTypes}
+                edges={edges}
+                fitView={true}
+                panOnDrag={false}
+                zoomOnScroll={false}
+                zoomOnPinch={false}
+                zoomOnDoubleClick={false}
+            />
+        </div>
+    );
+}
+
+export default SkillTreeContent;
--- src/components/SkillNode.tsx
+++ src/components/SkillNode.tsx
@@ -6,16 +6,30 @@ export default function SkillNode ({ data }: NodeProps) {
             <Handle 
                 type="target"  
                 position={Position.Top} 
-                style={{ opacity: 0, visibility: 'hidden' }} 
+                style={{ opacity: 0, visibility: 'hidden' }}
+                id="top-target"
+            />
+            <Handle 
+                type="source"  
+                position={Position.Top} 
+                style={{ opacity: 0, visibility: 'hidden' }}
+                id="top-source"
             />
             <div className="skill">
                 {data.label as string} 
                 <div className="tooltip">{data.toolTip as string}</div>
             </div>
             <Handle 
+                type="target"  
+                position={Position.Bottom} 
+                style={{ opacity: 0, visibility: 'hidden' }}
+                id="bottom-target"
+            />
+            <Handle 
                 type="source"  
                 position={Position.Bottom} 
                 style={{ opacity: 0, visibility: 'hidden' }}
+                id="bottom-source"
             />
         </>
     )
