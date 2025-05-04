47563460f17c2619feccd4521b39fe1872edcb91
NeptuneRjo <rjo9000@outlook.com>
2025-05-03 21:02:30 -0400

    Updated folder name and page export

--- src/app/dead-end/page.tsx
+++ src/app/dead-end/page.tsx
@@ -1,4 +1,4 @@
-export default function SkillTreePage() {
+export default function DeadEndPage() {
     return (
         <div className="stan-width"> 
             <h1 className="text-center"> This Page has not yet been made, please return home </h1>
