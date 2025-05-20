8c2502da231131e4b190ddb9b7153cb0938087dc
NeptuneRjo <rjo9000@outlook.com>
2025-05-20 17:16:23 -0400

    Implemented login form client component

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,14 +1,11 @@
+import AuthForm from './components/AuthForm';
+
 export default function AuthPage() {
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen mb 4">
         <h1 className="mb-4" >Enter Player ID</h1>
-        <form action="/">
-          <input
-            name="Player-ID"
-            placeholder="Player ID"
-            className="mb-4"
-          />
-        </form>
+        <AuthForm />
       </div>
     );
 }
