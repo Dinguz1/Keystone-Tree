1756005814fb08532352afc336d1b5d6b43915e8
NeptuneRjo <rjo9000@outlook.com>
2025-05-20 17:15:57 -0400

    Created api route to handle setting authentication cookie

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function POST (req: NextRequest, res: NextResponse) {
+    const { playerId } = await req.json()
+    console.log('playerId', playerId)
+    const cookieStore = await cookies()
+
+    // // Create a cookie storing the playerId that expires in 1 week
+    cookieStore.set('playerId', playerId, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 7,
+        secure: true
+    })
+
+    // console.log('playerId: ', cookieStore.get('playerId'))
+
+    return NextResponse.json({ message: `User with id ${playerId} authenticated`}, { status: 200 })
+}
