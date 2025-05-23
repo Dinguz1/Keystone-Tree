648b484bfbd29741f0d431c6cc9a58d48377c223
NeptuneRjo <rjo9000@outlook.com>
2025-05-23 15:10:38 -0400

    Updated tailwind config for heroui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ const {heroui} = require("@heroui/theme");
 module.exports = {
   content: [
     "./node_modules/@heroui/theme/dist/components/popover.js",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
