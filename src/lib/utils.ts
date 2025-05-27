a06edcb9ddd4785f19014a62583a05494003f173
NeptuneRjo <rjo9000@outlook.com>
2025-05-27 01:54:54 -0400

    Added auth level util after shadcn override

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * Returns an index depending on the matching ID-
+ * If there are no matching IDs, returns -1
+ * @param id 
+ * @returns number
+ */
+export const getAuthLevel = (id: string | null): number => {
+  switch (id) {
+    case '1235':
+      return 1
+    case '1235GM':
+      return 2
+    default:
+      return -1
+  }
+}
