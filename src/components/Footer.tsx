6568b3332bbc80e12926bec689a6c5712b23c715
NeptuneRjo <rjo9000@outlook.com>
2025-07-08 16:23:44 -0400

    Created footer for mobile; implement social svgs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,75 @@
+"use client";
+import useWindowDimensions from "@/lib/hooks/useWIndowDimension";
+import Image from 'next/image'
+import Link from "next/link";
+
 const Footer = () => {
+    const { width } = useWindowDimensions()
+
+    let logoHeight = 32
+    let logoWidth = 32
+
+    if (width !== undefined && width >= 1024) {
+        logoHeight = 36
+        logoWidth = 36
+    } else if (width !== undefined && width >= 1536) {
+        logoHeight = 42
+        logoWidth = 42
+    }
+
     return (
-        <footer className="flex flex-col items-center p-6 gap-4 xl:p-6 bg-(--bg-secondary) md:flex-row md:p-4">
+        <footer className="flex flex-col items-center p-6 gap-6 xl:p-6 bg-(--bg-secondary) md:flex-row md:p-4">
             <div className="w-full flex flex-col md:gap-2 md:text-center">
-                <h4 className="text-lg md:text-base">Contact Me</h4>
-                <p className="text-base">Contact Info for the Developer Goes Here</p>
+                <h4 className="hidden text-lg md:text-base">Socials</h4>
+                <ul className="flex justify-center md:grid grid-cols-6 md:w-60 gap-y-6 gap-6">
+                    <li className="flex col-span-2 justify-center">
+                        <Link href="#" className="flex">
+                            <Image src="/instagram.svg" alt="Instagram Logo" height={logoHeight} width={logoWidth} />
+                        </Link>
+                    </li>
+                    <li className="flex col-span-2 justify-center">
+                        <Link href="#" className="flex">
+                            <Image src="/youtube.svg" alt="Youtube Logo" height={logoHeight} width={logoWidth} />
+                        </Link>
+                    </li>
+                    <li className="flex col-span-2 justify-center">
+                        <Link href="#" className="flex">
+                            <Image src="/discord.svg" alt="Discord Logo" height={logoHeight} width={logoWidth} />
+                        </Link>
+                    </li>
+                    <li className="flex col-span-2 col-start-2 justify-center">
+                        <Link href="#" className="flex">
+                            <Image src="/reddit.svg" alt="Reddit Logo" height={logoHeight} width={logoWidth} />
+                        </Link>
+                    </li>
+                    <li className="flex col-span-2 col-start-4 justify-center">
+                        <Link href="#" className="flex">
+                            <Image src="/twitter.svg" alt="Twitter Logo" height={logoHeight} width={logoWidth} />
+                        </Link>
+                    </li>
+                </ul>
             </div>
-            <div className="hidden md:inline bg-(--bg-muted) w-[3px] h-[75px]" />
-            <div className="flex flex-col w-full md:text-center md:gap-2">
-                <h4 className="text-lg md:text-base">Keystone TTRPG</h4>
-                <p className="text-base">&copy; 2025 Project Keystone. All rights reserved.</p>
+            <div className="divider" />
+            <div className="w-full flex flex-col md:gap-2 md:text-center">
+                <h4 className="hidden text-lg md:text-base">Contact Me</h4>
+                {/* <p className="text-base">Contact Info for the Developer Goes Here</p> */}
+                <ul className="px-2 flex gap-4">
+                    <li className="text-base">
+                        <span className="hidden">Email:</span> keystonettrpg@guild.com
+                    </li>
+                    <li className="text-base">
+                        <span className="hidden">Phone Number:</span> 
+                        (555)-555-4567
+                    </li>
+                </ul>
+            </div>
+            <div className="divider" />
+            <div className="flex flex-row-reverse gap-4 md:text-center">
+                {/* <h4 className="text-lg md:text-base">Keystone TTRPG</h4> */}
+                <div className="w-50">
+                    <Image src="/logo.svg" alt="Keyston TTRPG Logo" fill={true} className=""/>
+                </div>
+                <p className="text-base text-center !font-thin w-30">&copy; 2025 Project Keystone. All rights reserved.</p>
             </div>
         </footer>
     );
