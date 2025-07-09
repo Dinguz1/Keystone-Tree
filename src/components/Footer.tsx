764c2938cc0f2aeee16341e7b83b499db1c0b621
NeptuneRjo <rjo9000@outlook.com>
2025-07-09 18:37:50 -0400

    Updated mobile styling to use tailwind components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,31 +18,31 @@ const Footer = () => {
     }
 
     return (
-        <footer className="flex flex-col items-center p-6 gap-6 xl:p-6 bg-(--bg-secondary) md:flex-row md:p-4">
-            <div className="w-full flex flex-col md:gap-2 md:text-center">
+        <footer className="grid grid-cols-1 grid-rows-3 p-6 bg-(--bg-secondary)">
+            <div className="footer-section md:gap-2 md:text-center">
                 <h4 className="hidden text-lg md:text-base">Socials</h4>
                 <ul className="flex justify-center md:grid grid-cols-6 md:w-60 gap-y-6 gap-6">
-                    <li className="flex col-span-2 justify-center">
+                    <li className="socials-item">
                         <Link href="#" className="flex">
                             <Image src="/instagram.svg" alt="Instagram Logo" height={logoHeight} width={logoWidth} />
                         </Link>
                     </li>
-                    <li className="flex col-span-2 justify-center">
+                    <li className="socials-item">
                         <Link href="#" className="flex">
                             <Image src="/youtube.svg" alt="Youtube Logo" height={logoHeight} width={logoWidth} />
                         </Link>
                     </li>
-                    <li className="flex col-span-2 justify-center">
+                    <li className="socials-item">
                         <Link href="#" className="flex">
                             <Image src="/discord.svg" alt="Discord Logo" height={logoHeight} width={logoWidth} />
                         </Link>
                     </li>
-                    <li className="flex col-span-2 col-start-2 justify-center">
+                    <li className="socials-item col-start-2">
                         <Link href="#" className="flex">
                             <Image src="/reddit.svg" alt="Reddit Logo" height={logoHeight} width={logoWidth} />
                         </Link>
                     </li>
-                    <li className="flex col-span-2 col-start-4 justify-center">
+                    <li className="socials-item col-start-4">
                         <Link href="#" className="flex">
                             <Image src="/twitter.svg" alt="Twitter Logo" height={logoHeight} width={logoWidth} />
                         </Link>
@@ -50,10 +50,10 @@ const Footer = () => {
                 </ul>
             </div>
             <div className="divider" />
-            <div className="w-full flex flex-col md:gap-2 md:text-center">
+            <div className="footer-section md:gap-2 md:text-center">
                 <h4 className="hidden text-lg md:text-base">Contact Me</h4>
                 {/* <p className="text-base">Contact Info for the Developer Goes Here</p> */}
-                <ul className="px-2 flex gap-4">
+                <ul className="px-2 flex justify-between">
                     <li className="text-base">
                         <span className="hidden">Email:</span> keystonettrpg@guild.com
                     </li>
@@ -64,7 +64,7 @@ const Footer = () => {
                 </ul>
             </div>
             <div className="divider" />
-            <div className="flex flex-row-reverse gap-4 md:text-center">
+            <div className="footer-section flex-row-reverse justify-between md:text-center">
                 {/* <h4 className="text-lg md:text-base">Keystone TTRPG</h4> */}
                 <div className="w-50">
                     <Image src="/logo.svg" alt="Keyston TTRPG Logo" fill={true} className=""/>
